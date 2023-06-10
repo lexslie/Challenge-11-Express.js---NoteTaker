e27a456561e1afbe671297152d2ab5e8a697a465
lexslie <lesliiee727@gmail.com>
2023-06-09 23:27:17 -0700

    Update server.js

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const PORT = process.env.PORT || 3001;
+const express = require("express");
+const app = express ();
+const fs = require("fs");
+const path = require("path");
+const api = require("./routes/api");
+const html = require("./routes/html");
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use("./api", api);
+app.use("/", html);
+
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}! ✏️`)
+});
